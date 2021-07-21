4a91c9ab9a9204cbc5b8c87a54bb0488cc8034c2
Krishna Sharma <Krrish.rbs@gmail.com>
2021-07-21 09:45:27 +0530

    Updated CSS.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import type { AppProps } from "next/app";
 import { wrapper } from "../redux/store";
 import PageBodyContainer from "../components/PageBodyContainer";
 
-import "../styles/app.styles.css";
+import "./index.css"
 
 function App({ Component, pageProps }: AppProps) {
   return (
